637fb1a7576c007cd86cbfdc2fedcaef55cc8c3b
mlhltnk <109691158+mlhltnk@users.noreply.github.com>
2024-01-06 19:19:02 +0300

    06.01.2024

--- Blog_MVC/ViewComponents/Writer/WriterMessageNotification.cs
+++ Blog_MVC/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +9,23 @@ namespace Blog_MVC.ViewComponents.Writer
     {
 		Message2Manager mm = new Message2Manager(new EfMessage2Repository());
 
+        Context c = new Context();
+
         //WriterDashboard sayfasında sağ üstteki mesaj bildirimleri kısmını getiriyor
 		public IViewComponentResult Invoke() //invoke=çağırma
         {
-            int id = 2;
-            var values = mm.GetInboxListByWriter(id);
+            //SİSTEME AUTHENTİCE OLANA GÖRE VERİ GETİRME**************************
+            var username = User.Identity.Name;
+
+            //Users; Appuser tablosunu ifade eder. 
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();    //kullanıcı adını kullanarak mail adresini çektim  //Users tablosunda UserName özelliği username ile eşleşen bir kullanıcının e-posta adresini bulur
+
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();  //Writers tablosunda WriterMail özelliği usermail ile eşleşen bir yazarın WriterID değerini bulur.
+
+            //**********************************************************************
+
+            var values = mm.GetInboxListByWriter(writerID);
+
             return View(values);
         }
 
